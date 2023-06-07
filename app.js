05c12a8b1b925a220b6e0bac9366f729d3b3f027
Ingrid Brito <brito.ingrid2202@gmail.com>
2023-06-07 15:13:28 -0300

    Adicionei o checklist.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist.js')
+const app = express();
+
+app.use(express.json())
+
+app.use('/checklists', checklistRouter)
+app.listen(3000, ()=>{
+    console.log('Servidor foi iniciado');
+})
